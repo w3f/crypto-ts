98e7fc959a7af79760d3422861a582578cc61a9c
pantelis <pampatzoglou@gmail.com>
2020-05-08 15:25:35 +0300

    init

---
+++ test/crypto.ts
@@ -0,0 +1,15 @@
+import { should } from 'chai';
+
+import { crypto } from '../src/';
+
+should();
+
+
+describe('crypto', () => {
+    it('Test crypto', () => {
+        const c = crypto();
+
+        //((): void => { logger.info('hello world') }).should.not.throw();
+
+    });
+});
