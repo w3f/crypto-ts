b1039117223354fcc57729abde5b3aaf09dcf784
pampatzoglou <15629182+pampatzoglou@users.noreply.github.com>
2020-05-14 11:41:53 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Keyring from '@polkadot/keyring';
 import { mnemonicGenerate, mnemonicToSeed, mnemonicValidate } from '@polkadot/util-crypto';
 import process from 'process';
-import u8aToHex from '@polkadot/util/u8a/toHex';
+import u8aToHex from '@polkadot/util';
 import { waitReady } from '@polkadot/wasm-crypto';
 
 
